2201326a394534fae42c1775302009050df1edb4
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-18 15:11:47 -0600

    demo complete.

--- ExtensionMethodDemo/Program.cs
+++ ExtensionMethodDemo/Program.cs
@@ -1,12 +1,62 @@
-﻿namespace ExtensionMethodDemo
+﻿namespace ExtensionMethodDemo;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
+        HotelRoomModel room = new HotelRoomModel();
 
+        room.TurnOnAir().SetTemperature(72).OpenShades();
 
-            Console.ReadLine();
-        }
+        room.TurnOffAir().CloseShades();
+
+        "hello world".PrintToConsole();
+
+        Console.ReadLine();
+    }
+}
+
+public class HotelRoomModel
+{
+    public int Temperature { get; set; }
+    public bool IsAirRunning { get; set; }
+    public bool AreShadesOpen { get; set; }
+}
+
+public static class ExtensionSamples
+{
+    public static void PrintToConsole(this string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public static HotelRoomModel TurnOnAir(this HotelRoomModel room)
+    {
+        room.IsAirRunning = true;
+        return room;
+    }
+
+    public static HotelRoomModel TurnOffAir(this HotelRoomModel room)
+    {
+        room.IsAirRunning = false;
+        return room;
+    }
+
+    public static HotelRoomModel SetTemperature (this HotelRoomModel room, int temperature)
+    {
+        room.Temperature = temperature;
+        return room;
+    }
+
+    public static HotelRoomModel OpenShades(this HotelRoomModel room)
+    {
+        room.AreShadesOpen = true;
+        return room;
+    }
+
+    public static HotelRoomModel CloseShades(this HotelRoomModel room)
+    {
+        room.AreShadesOpen = false;
+        return room;
     }
 }
